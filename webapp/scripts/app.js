9210affd5c200a54a47db3c6184dda93cc5242d7
Florian Egner <egner.florian@gmail.com>
2015-03-15 20:14:44 +0100

    add google maps

--- webapp/scripts/app.js
+++ webapp/scripts/app.js
@@ -9,4 +9,10 @@
  * Main module of the application.
  */
 angular
-	.module('mavlink', []);
+	.module('mavlink', ['uiGmapgoogle-maps']).config(function(uiGmapGoogleMapApiProvider) {
+		uiGmapGoogleMapApiProvider.configure({
+			//    key: 'your api key',
+			v: '3.17',
+			libraries: 'weather,geometry,visualization'
+		});
+	})
--- webapp/scripts/controllers/main.js
+++ webapp/scripts/controllers/main.js
@@ -8,18 +8,39 @@
  * Controller of the mavlink
  */
 angular.module('mavlink')
-	.controller('MainCtrl', ["$scope", "$rootScope", "socket", function($scope, $rootScope, socket) {
+	.controller('MainCtrl', ["$scope", "$rootScope", "socket", "uiGmapGoogleMapApi", function($scope, $rootScope, socket, uiGmapGoogleMapApi) {
 
 		var localData = {};
-
 
 		$rootScope.$on('updateData', function(event, data) {
 			localData[data.type] = data;
 
 			$scope.$apply(function() {
 				$scope.data = localData;
+
+				if (localData.GLOBAL_POSITION_INT) {
+					var lat = $scope.parseGeo(localData.GLOBAL_POSITION_INT.message.lat);
+					var lon = $scope.parseGeo(localData.GLOBAL_POSITION_INT.message.lon);
+
+					$scope.map = {
+						center: {
+							latitude: lat,
+							longitude: lon
+						},
+						zoom: 8
+					};
+				}
 			});
 		});
 
+		window.getData = function() {
+			return localData;
+		}
+
+		$scope.parseGeo = function(geo) {
+			if (geo) {
+				return geo.toString().substring(0, 2) + '.' + geo.toString().substring(2);
+			}
+		}
 
 	}]);
