366703d73697ff3b6bf2e173d4a201467c039fd3
Florian Egner <egner.florian@gmail.com>
2015-06-10 20:16:36 +0200

    add video web socket

--- server.js
+++ server.js
@@ -40,3 +40,59 @@ http.listen(config.express_port, function() {
 io.on('connection', function(socket) {
 	console.log("Client connected");
 });
+
+// Video Websocket Server
+var videoWsServer = new (require('ws').Server)({port: config.video.WEBSOCKET_PORT});
+videoWsServer.on('connection', function(socket) {
+	// Send magic bytes and video size to the newly connected socket
+	// struct { char magic[4]; unsigned short config.video.width, config.video.height;}
+	var streamHeader = new Buffer(8);
+	streamHeader.write(config.video.STREAM_MAGIC_BYTES);
+	streamHeader.writeUInt16BE(config.video.width, 4);
+	streamHeader.writeUInt16BE(config.video.height, 6);
+	socket.send(streamHeader, {binary:true});
+
+	console.log( 'New WebSocket Connection ('+videoWsServer.clients.length+' total)' );
+	
+	socket.on('close', function(code, message){
+		console.log( 'Disconnected WebSocket ('+videoWsServer.clients.length+' total)' );
+	});
+});
+
+videoWsServer.broadcast = function(data, opts) {
+	for( var i in this.clients ) {
+		if (this.clients[i].readyState == 1) {
+			this.clients[i].send(data, opts);
+		}
+		else {
+			console.log( 'Error: Client ('+i+') not connected.' );
+		}
+	}
+};
+
+// HTTP Server to accept incomming MPEG Stream
+var videoStreamServer = require('http').createServer( function(request, response) {
+	var params = request.url.substr(1).split('/');
+
+	if( params[0] == config.video.STREAM_SECRET ) {
+		config.video.width = (params[1] || 320)|0;
+		config.video.height = (params[2] || 240)|0;
+		
+		console.log(
+			'Stream Connected: ' + request.socket.remoteAddress + 
+			':' + request.socket.remotePort + ' size: ' + config.video.width + 'x' + config.video.height
+		);
+		request.on('data', function(data){
+			videoWsServer.broadcast(data, {binary:true});
+		});
+	}
+	else {
+		console.log(
+			'Failed Stream Connection: '+ request.socket.remoteAddress + 
+			request.socket.remotePort + ' - wrong secret.'
+		);
+		response.end();
+	}
+}).listen(config.video.STREAM_PORT);
+
+console.log("run", "ffmpeg -s "+config.video.height+"x"+config.video.width+" -f "+config.video.format+" -i " + config.video.device + " -f mpeg1video -b 800k -r 30 http://localhost:"+config.video.STREAM_PORT+"/"+config.video.STREAM_SECRET+"/"+config.video.height+"/"+config.video.width+"/")
