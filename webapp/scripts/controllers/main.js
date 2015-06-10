30fa0be768395ed88cac3865c4114abba441976b
Florian Egner <egner.florian@gmail.com>
2015-06-10 19:07:49 +0200

    Revert "Altitude update"
    This reverts commit c64481f5f09c227b3e1e071ddec3f146b747f4c1.

--- webapp/scripts/controllers/main.js
+++ webapp/scripts/controllers/main.js
@@ -66,6 +66,18 @@ angular.module('mavlink')
 				return cur.toString().substring(0, 2) + '.' + cur.toString().substring(2);
 			}
 		}
+
+		$scope.setCubeAngleTo = function(x,y,z){
+			var Cube = $('#box');
+
+			x = x * 1000;
+			y = y * 1000;
+			z = z * 1000;
+
+			$(Cube).css("transform", "rotateX( "+ x +"deg) rotateY( "+ y +"deg) rotateZ( "+ z +"deg)");
+			console.log("x " + x + " y " + y + " z " + z);
+
+		}
 		
 
 	}]);
